1c5adcae4f1ca1ce7e37675cad5118f457226d7d
Riccardo <riccardo_london@live.it>
2019-03-24 10:50:29 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,7 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
 
   def change
-    create_table :students, :name, :string
-  end
+    create_table :students do |i|
+      i.string :name
 
 end
